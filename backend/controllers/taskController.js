3a147fa4e78e474f6a2eb99a6ef511c76ecbcfc5
Mateusz <mateusz.smardzewski.stud@pw.edu.pl>
2023-05-22 21:33:56 +0200

    getTaskByEmployeeName

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -48,3 +48,26 @@ exports.getEmployeeTask = (req, res, status)=>{
         });
     }
 }
+
+exports.getEmployeeTaskByName = (req, res, status)=>{
+    try{
+        const {employeeName} = req.params;
+        task.find({employeeName: employeeName}).populate('name').exec().then(tasks=>{
+            if(tasks!=undefined){
+                return res.status(200).json({
+                    tasks
+                });
+            }
+            else{
+                return res.status(400).json({
+                    error: "Error during get task"
+                })
+            }
+        })
+    }
+    catch(e){
+        return res.status(400).json({
+            error
+        });
+    }
+}
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {addNewTask, getEmployeeTask} = require('../controllers/taskController');
+const {addNewTask, getEmployeeTask, getEmployeeTaskByName} = require('../controllers/taskController');
 
 router.post('/addnewtask', addNewTask);
 router.get('/getemployeetask/:employeeId', getEmployeeTask);
+router.get('getemployeetaskbyname/:employeeName', getEmployeeTaskByName);
 
 module.exports = router;
