675dbb484ce98ddaacfa8a303583ed87e2083790
Mateusz <mateusz.smardzewski.stud@pw.edu.pl>
2023-05-22 21:30:08 +0200

    getAllEmployees and getEmployeeByName

--- backend/controllers/employeesController.js
+++ backend/controllers/employeesController.js
@@ -45,3 +45,26 @@ exports.getEmployeeById = (req, res, status)=>{
         });
     }
 }
+
+exports.getEmployeeByName = (req, res, status)=>{
+    try{
+        const {employeeName} = req.params;
+        signings.find({_name: employeeName}).exec().then(employee=>{
+            if(employee!=undefined){
+                return res.status(200).json({
+                    employee
+                })
+            }
+            else{
+                return res.status(400).json({
+                    error: "Error during get emplyee"
+                })
+            }
+        })
+    }
+    catch (e) {
+        return res.status(400).json({
+            error: e
+        });
+    }
+}
--- backend/routes/employeeRoute.js
+++ backend/routes/employeeRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {getEmployeeById} = require('../controllers/employeesController');
+const {getAllEmployees, getEmployeeById, getEmployeeByName} = require('../controllers/employeesController');
 
+router.get('/getallemploees', getAllEmployees);
 router.get('/employeebyid/:employeeId', getEmployeeById);
+router.get('/employeebyname/:employeeName', getEmployeeByName);
 
 module.exports = router;
