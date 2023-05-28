b938190506681635c864cf35b4b12fc31575af11
Mateusz <mateusz.smardzewski.stud@pw.edu.pl>
2023-05-28 23:02:29 +0200

    Mainmeal fixed

--- meals/Mainmeal.h
+++ meals/Mainmeal.h
@@ -10,20 +10,6 @@ class Mainmeal:public Meal{
     string type;
 
 public:
-    Mainmeal(string id, string shortname, string name, string description, bool availability, int productsCount, vector<Ingredient> ingredients,double price,string type):Meal(id, shortname, name, description, availability, productsCount, ingredients, price){
-        this -> type = type;
-        bool availabilityTemp = true;
-        for(int i=0; i<ingredients.size(); i++){
-            if(!ingredients[i].checkAvailability()){
-                availabilityTemp = false;
-                break;
-            }
-        }
-        availability = availabilityTemp;
-    }
-
-
-
-
-
+    Mainmeal(string id, string shortname, string name, string description, bool availability, int productsCount, vector<Ingredient> ingredients,double price,string type):Meal(id, shortname, name, description, availability, productsCount, ingredients, price){}
+    Mainmeal(string id, string shortname, string name, string description, bool availability, int productsCount, vector<string> ingredients,double price,string type):Meal(id, shortname, name, description, availability, productsCount, ingredients, price){}
 };
